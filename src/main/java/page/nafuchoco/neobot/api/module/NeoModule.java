b2a08407b581d88359bd2b125cceef4e1080621c
Nafu Satsuki <chocotto@nafuchoco.page>
2022-06-29 21:37:31 +0900

    Deprecate queueCommandRegister.

--- src/main/java/page/nafuchoco/neobot/api/module/NeoModule.java
+++ src/main/java/page/nafuchoco/neobot/api/module/NeoModule.java
@@ -91,7 +91,7 @@ public abstract class NeoModule implements Module {
 
     @Override
     public void queueCommandRegister() {
-        launcher.queueCommandRegister();
+        // do nothing
     }
 
     @Override
